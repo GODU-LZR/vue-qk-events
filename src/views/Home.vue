27d46803522cfe58e05d20293736982db544d9a1
GODU-LZR <2980560533@qq.com>
2025-01-13 04:59:17 +0800

    qiankun vue vue-route vue-x element-ui 图表组件 qiankun基座微应用框架整合

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<!-- vue-qk-events/src/views/Home.vue -->
+<template>
+  <div>
+    <h2>体育赛事模块首页</h2>
+    <el-button type="primary" @click="showMessage">点击测试</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventsHome',
+  methods: {
+    showMessage() {
+      this.$message('体育赛事子应用的 ElementUI 按钮点击了！');
+    }
+  }
+}
+</script>
